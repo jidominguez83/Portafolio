53e75b84dfca5b5c8d4420a14448d76576d8b6bc
jidominguez83 <ivan_@LAPTOP-H07L0F69>
2022-10-06 22:39:16 -0500

    Se agregó la sección de trabajos recientes.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,47 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
+        }
+
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Link = "https://amazon.com",
+                    ImagenURL = "/images/amazon.png"
+                },
+
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Página de noticias en React",
+                    Link = "https://nytimes.com",
+                    ImagenURL = "/images/nyt.png"
+                },
+
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link = "https://reddit.com",
+                    ImagenURL = "/images/reddit.png"
+                },
+
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para comprar videojuegos",
+                    Link = "https://store.steampowered.com",
+                    ImagenURL = "/images/steam.png"
+                }
+            };
         }
 
         public IActionResult Privacy()
