8df11976a1a34f82918e05ae79b8b12a6e20b307
jidominguez83 <domingueztorres.ji@gmail.com>
2023-04-03 09:00:33 -0600

    La aplicación ya contiene la sección de contacto y envía correos electrónicos.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Servicios;
 using System.Diagnostics;
 
 namespace Portafolio.Controllers
@@ -7,58 +8,43 @@ namespace Portafolio.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos repositorioProyectos;
+        private readonly IServicioEmail servicioEmail;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos, IServicioEmail servicioEmail)
         {
             _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
+            this.servicioEmail = servicioEmail;
         }
 
         public IActionResult Index()
         {
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
         }
 
-        private List<Proyecto> ObtenerProyectos()
+        public IActionResult Proyectos()
         {
-            return new List<Proyecto>()
-            {
-                new Proyecto
-                {
-                    Titulo = "Amazon",
-                    Descripcion = "E-Commerce realizado en ASP.NET Core",
-                    Link = "https://amazon.com",
-                    ImagenURL = "/images/amazon.png"
-                },
+            var proyectos = repositorioProyectos.ObtenerProyectos();
 
-                new Proyecto
-                {
-                    Titulo = "New York Times",
-                    Descripcion = "Página de noticias en React",
-                    Link = "https://nytimes.com",
-                    ImagenURL = "/images/nyt.png"
-                },
-
-                new Proyecto
-                {
-                    Titulo = "Reddit",
-                    Descripcion = "Red social para compartir en comunidades",
-                    Link = "https://reddit.com",
-                    ImagenURL = "/images/reddit.png"
-                },
-
-                new Proyecto
-                {
-                    Titulo = "Steam",
-                    Descripcion = "Tienda en línea para comprar videojuegos",
-                    Link = "https://store.steampowered.com",
-                    ImagenURL = "/images/steam.png"
-                }
-            };
+            return View(proyectos);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contacto(ContactoViewModel contactoViewModel)
+        {
+            await servicioEmail.Enviar(contactoViewModel);
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias()
         {
             return View();
         }
